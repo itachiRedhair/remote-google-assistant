fb8e9526989e7347fa75d3a472b4a0213cedbf28
itachiRedhair <akshay.milmile@gmail.com>
2018-07-18 08:44:07 +0530

    Initial

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ const app = express();
 
 const GoogleAssistant = require('./googleAssistant.js');
 
-const deviceCredentials = {
-  client_secret: '16Z1ksEPoLCsBGIq0Oyv982Z',
-  refresh_token: '1/kRQ8htNpTFWZaTdWTBHnZmlBSVKucNDgWVN9MJzrEHQ',
-  scopes: ['https://www.googleapis.com/auth/assistant-sdk-prototype'],
-  client_id: '1026086369065-pt5sedk4mmb5d5gudiiiou5cf14vo7ab.apps.googleusercontent.com',
-  token_uri: 'https://accounts.google.com/o/oauth2/token',
-};
+// const deviceCredentials = {
+//   client_secret: '16Z1ksEPoLCsBGIq0Oyv982Z',
+//   refresh_token: '1/kRQ8htNpTFWZaTdWTBHnZmlBSVKucNDgWVN9MJzrEHQ',
+//   scopes: ['https://www.googleapis.com/auth/assistant-sdk-prototype'],
+//   client_id: '1026086369065-pt5sedk4mmb5d5gudiiiou5cf14vo7ab.apps.googleusercontent.com',
+//   token_uri: 'https://accounts.google.com/o/oauth2/token',
+// };
 
 // const homedir = require('homedir');
 // const deviceCredentials = require(`${homedir()}/.config/google-oauthlib-tool/credentials.json`);
+const deviceCredentials = require('./deviceCredentials.json');
 
 const CREDENTIALS = {
   client_id: deviceCredentials.client_id,
@@ -37,6 +38,9 @@ const getAssistantResponse = async prompt => {
 
 app.get('/', async (req, res) => {
   try {
+    if (!req.query.query || !req.query.query === '') {
+      throw Error('Please provide query.');
+    }
     const response = await getAssistantResponse(req.query.query);
     res.send(response);
   } catch (err) {
